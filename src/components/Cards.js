396f7a80617897f6e7cdede3f030207d3e0021e9
Joel Tan <joeltanjh99@gmail.com>
2021-05-25 09:37:34 +0800

    Home page edit
    Edited nav-bar, home page main font, home page video, some of the other stock photos, some words.
     I also imported bootstrap into app,js.  The website loads as normal. 2hrs.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
 
 function Cards() {
   return (
-    <div className='cards'>
+    <div className="cards">
       <h1>Check out these USEFUL articles!</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <CardItem
-              src='images/img-9.jpg'
-              text='Is the stock market outdated?'
-              label='Stocks'
-              path='/services'
+              src="images/stockmarket.jpg"
+              text="Introduction to the stock market"
+              label="Stocks"
+              path="/services"
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='The future of finance'
-              label='Crypocurrency'
-              path='/services'
+              src="images/dogecoin.jpeg"
+              text="The future of finance"
+              label="Crypocurrency"
+              path="/services"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/img-3.jpg'
-              text='Technical Analysis tutorial'
-              label='Options'
-              path='/services'
+              src="images/optionstrading.jpg"
+              text="Technical Analysis tutorial"
+              label="Options"
+              path="/services"
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Is it profitable to invest in property?'
-              label='REIT'
-              path='/products'
+              src="images/reits.jpg"
+              text="Is it profitable to invest in property?"
+              label="REIT"
+              path="/products"
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='What are securities?'
-              label='securities'
-              path='/sign-up'
+              src="images/accounting.jpg"
+              text="Fundamental analysis tutorial"
+              label="Securities Research"
+              path="/sign-up"
             />
           </ul>
         </div>
