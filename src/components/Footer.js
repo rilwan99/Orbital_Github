2df76f93845f5866a0a5caa21737f9a707dae0cd
Muhammad Rilwan <rilwanwild@gmail.com>
2021-05-26 22:10:20 +0800

    Footer and Header edit
    Changed up the header and footer, I think we are ready for technical POC video making

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,9 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
+Join the InvestmentGuru community and connect with like-minded individuals!        </p>
         <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
+          Your chance to learn more and help others learn as well
         </p>
         <div className='input-areas'>
           <form>
@@ -21,7 +20,7 @@ function Footer() {
               type='email'
               placeholder='Your Email'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle='btn--outline'>Sign up</Button>
           </form>
         </div>
       </section>
@@ -31,31 +30,19 @@ function Footer() {
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
             <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
             <Link to='/'>Twitter</Link>
           </div>
         </div>
@@ -64,11 +51,10 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
+              INVESTMENTGURU
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
