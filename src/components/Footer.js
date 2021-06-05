7fcccaabc1cf2bfbe6b8cae4eba26c1ef7ec800c
Muhammad Rilwan <rilwanwild@gmail.com>
2021-06-05 14:47:25 +0800

    Some cleaning up
    Tidied the code abit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,12 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-<<<<<<< HEAD
-    <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join the Investment Gurus newsletter to receive stock market news
-        </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-=======
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
 Join the InvestmentGuru community and connect with like-minded individuals!        </p>
         <p className='footer-subscription-text'>
           Your chance to learn more and help others learn as well
->>>>>>> master
         </p>
         <div className="input-areas">
           <form>
@@ -30,11 +20,7 @@ Join the InvestmentGuru community and connect with like-minded individuals!     
               type="email"
               placeholder="Your Email"
             />
-<<<<<<< HEAD
-            <Button buttonStyle="btn--outline">Subscribe</Button>
-=======
             <Button buttonStyle='btn--outline'>Sign up</Button>
->>>>>>> master
           </form>
         </div>
       </section>
@@ -42,55 +28,12 @@ Join the InvestmentGuru community and connect with like-minded individuals!     
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>About Us</h2>
-<<<<<<< HEAD
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-=======
             <Link to='/sign-up'>How it works</Link>
             <Link to='/'>Testimonials</Link>
             <Link to='/'>Investors</Link>
->>>>>>> master
           </div>
           <div class="footer-link-items">
             <h2>Contact Us</h2>
-<<<<<<< HEAD
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
-          </div>
-        </div>
-        <div className="footer-link-wrapper">
-          <div class="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
-          </div>
-          <div class="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
-          </div>
-        </div>
-      </div>
-      <section class="social-media">
-        <div class="social-media-wrap">
-          <div class="footer-logo">
-            <Link to="/" className="social-logo">
-              INVESTMENT GURUS
-              <i class="fab fa-typo3" />
-            </Link>
-          </div>
-          <small class="website-rights">Investment Gurus © 2020</small>
-          <div class="social-icons">
-=======
             <Link to='/'>Contact</Link>
             <Link to='/'>Support</Link>
           </div>
@@ -113,7 +56,6 @@ Join the InvestmentGuru community and connect with like-minded individuals!     
             </Link>
           </div>
           <div class='social-icons'>
->>>>>>> master
             <Link
               class="social-icon-link facebook"
               to="/"
