34b207028730b20b1bbaf1fd9515eaeb5f7a6cfc
Joel Tan <joeltanjh99@gmail.com>
2021-06-27 22:27:42 +0800

    Fixed Query Issue
    Fixed issue where app cannot read query

--- src/components/pages/Forum/search.js
+++ src/components/pages/Forum/search.js
@@ -5,18 +5,18 @@ import "./post.css";
 
 function Search() {
   const [query, setQuery] = useState("");
-  const [result, setResult] = useState([]);
   const [posts, setPosts] = useState([]);
   const db = firebase.firestore();
 
   //preventDefault --> Receive the query from user --> Load result from database --> render the new results
   const handleInputChange = (event) => {
-    event.preventDefault();
+    //event.preventDefault();
     setQuery(event.target.value);
     console.log("Updated Query : " + query);
   };
 
-  const handleQuery = () => {
+  const handleQuery = (event) => {
+    event.preventDefault();
     loadData();
     renderPosts();
   };
@@ -30,44 +30,6 @@ function Search() {
     console.log("UseEffect-renderposts is called");
   }, []);
 
-  //useEffect();
-
-  /*
-    const handleQuery = () => {
-        const db = firebase.firestore();
-        db.collection("forumpost").get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                posts.push({
-                    data: doc.data(),
-                    id: doc.id
-                });
-            });
-            this.setState({
-                data: posts
-            });
-        });
-    }*/
-
-  //After setting the query result,
-  //result should contain an array of Post object literals.
-  /*
-  const loadData = (query) => {
-    let posts = [];
-    db.collection("Posts")
-      .where("title", "==", query)
-      .limit(15)
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach(function (doc) {
-          posts.push({
-            data: doc.data(),
-            //id: doc.id,
-          });
-        });
-        setResult(posts);
-      });
-  };*/
-
   //This sometimes works, and sometimes does not work.
   //No idea why, but perhaps due to asynchronous programming.
 
@@ -76,71 +38,33 @@ function Search() {
   const loadData = () => {
     let posts = [];
     db.collection("forumpost")
-      .where("title", "==", "Options Pricing")
-      //.limit(15)
+      .where("title", "==", query)
+      .limit(15)
       .get()
       .then((querySnapshot) => {
         console.log("checking database...");
         if (querySnapshot.empty) {
+          console.log("Query:", query);
           console.log("No results");
           return <p>No results found.</p>;
         }
         querySnapshot.forEach((doc) => {
           console.log(doc.id, " => ", doc.data());
-          //setResult(doc.data(), ...result);
-          //console.log(result);
           posts.push(doc.data());
-          //setResult(posts);
+          setPosts(posts);
           console.log(posts);
-          //doc.data() is never undefined for query doc snapshots
         });
       });
 
-    //console.log(result);
     renderPosts();
   };
-
-  /*
-  const loadData = async() => {
-      const response = db.collection("forumpost").where("title", "==", query);
-      const data = await response.get();
-      data.docs.forEach(item=>{
-
-      })
-
-  }*/
 
   //suspect sth wrong here
   /*TODO */
   const renderPosts = () => {
     console.log("Helloworld RENDERING");
     console.log(posts);
-<<<<<<< Updated upstream
-=======
-    return posts.map((post) => {
-      return (
-        <div>
-          <p>HELLOOOOOOOOOOOOOO</p>
-          <Post
-            title={post.title}
-            text={post.text}
-            name={post.name}
-            time={post.time}
-          />
-        </div>
-      );
-    });
->>>>>>> Stashed changes
   };
-
-  /* Example of rendering
-    <TrendingCardItem
-                src="https://images.financialexpress.com/2021/03/Stock-REU2-13.jpg"
-                text="Stock Market outlook 2021"
-                subtext="Morganstanley"
-                label="Stocks"
-                path="https://www.morganstanley.com/ideas/stock-market-outlook-2021"
-              />*/
 
   return (
     <>
@@ -150,9 +74,9 @@ function Search() {
           <input
             className="search-input"
             value={query}
+            type="text"
+            id="query"
             onChange={handleInputChange}
-            name="query"
-            label="query"
             placeholder="Search for a title here."
           />
           <button type="submit" onClick={handleQuery}>
@@ -160,7 +84,6 @@ function Search() {
           </button>
         </label>
       </form>
-<<<<<<< Updated upstream
       <div>
         {posts.map((post) => (
           <Post
@@ -171,9 +94,6 @@ function Search() {
           />
         ))}
       </div>
-=======
-      <div>{renderPosts}</div>
->>>>>>> Stashed changes
     </>
   );
 }
